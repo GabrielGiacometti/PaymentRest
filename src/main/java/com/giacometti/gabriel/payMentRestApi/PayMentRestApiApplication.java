cbcacdfbc3660ee8ee6b65c4272cf89562da89d3
gabrielgiacometti <henrique.giacometti@aluno.ifsp.edu.br>
2023-11-28 15:43:40 -0300

    Initialiser Project

---
+++ src/main/java/com/giacometti/gabriel/payMentRestApi/PayMentRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.giacometti.gabriel.payMentRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMentRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMentRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giacometti/gabriel/payMentRestApi/PayMentRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giacometti.gabriel.payMentRestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMentRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
